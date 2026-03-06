0837e28d8f75bed56442a6c88b543c45da98bb16
rajaexam13-afk <rajaexam13@gmail.com>
2026-03-06 19:27:40 +0530

    Create main.py

---
+++ services/metadata_service/main.py
@@ -0,0 +1,8 @@
+services/metadata_service/main.py
+services/metadata_service/models.py
+services/metadata_service/crud.py
+services/metadata_service/database.py
+services/metadata_service/schemas.py
+services/metadata_service/routers/dimensions.py
+services/metadata_service/routers/attributes.py
+services/metadata_service/routers/members.py
